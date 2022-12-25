e462b862799c09ca92faf645c5263e6c9113b16f
ppCoffee <646439950@qq.com>
2022-12-26 04:06:23 +0800

    update

---
+++ projectMonitor/src/test/java/com/evmtv/boot/ProjectMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evmtv.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
